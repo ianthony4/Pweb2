8cc0746444eb567d161173374ff824c4dc6b5ab8
ianthony4 <achaisa@unsa.edu.pe>
2022-04-28 11:24:12 -0500

    HolaMundo.java funciona, se agrego la firma final

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo {
     public static void main(String[] args){
         System.out.println("Hola Mundo");
         System.out.println("Programacion Web 2");
+        System.out.println("Saludos de Anthony Chaisa Fernandez");
     }
 }
